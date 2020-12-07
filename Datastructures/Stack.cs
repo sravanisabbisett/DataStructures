a2033108cc4a9c72b10c0feea14b112d9ce2928e
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-07 18:07:49 +0530

    ADDED:StackPush method to push the element in to stack

---
+++ Datastructures/Stack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures
+{
+    public class Stack
+    {
+        LinkedList linkedList;
+
+        public Stack()
+        {
+            linkedList = new LinkedList();
+        }
+
+        /// <summary>
+        /// Pushes the elements in to stack.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            linkedList.Add(data);
+        }
+
+    }
+}
---
+++ LinkedList/StackTest.cs
@@ -0,0 +1,16 @@
+﻿using Datastructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkedListTest
+{
+    [TestClass]
+    class StackTest
+    {
+        Stack stack;
+
+        public StackTest()
+        {
+            stack = new Stack();
+        }
+    }
+}
