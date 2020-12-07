34022809dd1678fd42540e28ea903bd6c250d5e1
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-07 19:46:48 +0530

    ADDED:Queue Enqueue method to add the elements in queue

---
+++ Datastructures/Queue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures
+{
+    public class Queue
+    {
+        LinkedList LinkedList;
+        Stack stack;
+        public Queue()
+        {
+            LinkedList = new LinkedList();
+            stack = new Stack();
+        }
+
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (IsEmpty())
+            {
+                LinkedList.Add(data);
+            }
+            else
+            {
+                LinkedList.Append(data);
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return stack.IsEmpty();
+        }
+    }
+}
